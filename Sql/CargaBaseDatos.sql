0bf8a5e533fe727731bc8eba1a054dcd9fd5eaee
Ignacio <ledoato@gmail.com>
2025-06-25 20:15:52 -0300

    BD

--- Sql/CargaBaseDatos.sql
+++ Sql/CargaBaseDatos.sql
@@ -11,15 +11,30 @@ SELECT * FROM pedido;
 SELECT * FROM pedido_detalle;
 
 
-
-
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
 --CARGA DE USUARIOS NUEVOS
 INSERT INTO usuario(id, nombre, apellido, email, telefono, direccion, pass, tipo_usuario) VALUES
 (NULL, "Ignacio", "Ledo", "ignacioledo@uca.edu.ar", "1155244402", "Ibera 2865", "1234", "usuario"), 
 (NULL, "Alejo", "Pisani", "alejopisani@uca.edu.ar", "112435432", "St.Thomas 111", "4321", "admin"),
 (NULL, "Ivan", "Finamore", "ivanfinamore@uca.edu.ar", "1187453782", "Libertador 111", "0987", "usuario");
 
+INSERT INTO usuario VALUES
+(4, "Tomás", "Fernández", "tomas.fernandez@uca.edu.ar", "1112345678", "Avenida 742", "6575", "usuario"),
+(5, "Julieta", "Gómez", "julieta.gomez@uca.edu.ar", "1122334455", "San Martín 2045", "8345", "usuario"),
+(6, "Mateo", "Martínez", "mateo.martinez@uca.edu.ar", "1133445566", "Belgrano 889", "7658", "usuario"),
+(7, "Bianca", "Sosa", "bianca.sosa@uca.edu.ar", "1144556677", "Rivadavia 3012", "6759", "usuario"),
+(8, "Joaquín", "Herrera", "joaquin.herrera@uca.edu.ar", "1155667788", "Corrientes 1743", "6765", "usuario"),
+(9, "Delfina", "Molina", "delfina.molina@uca.edu.ar", "1166778899", "Castelli 501", "8923", ""),
+(10, "Nicolás", "Castro", "nicolas.castro@uca.edu.ar", "1177889900", "Mariano Castex 283", "1549", "usuario");
 
+UPDATE usuario SET tipo_usuario = 'usuario' WHERE id = 9;
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
 --CARGA DE PRODUCTOS BASE
 INSERT INTO producto(id, nombre, marca, descripcion, precio, stock, imagen, categoria, sabor) VALUES
 (NULL, "Whey Protein", "Ena", "Whey protein concentrate (WPC): perfil completo de aminoácidos esenciales, brindando la mejor calidad de proteínas para aumentar la energía durante el entrenamiento.", 38900, 50, "https://www.enasport.com/cdn/shop/files/TM_WP_2lbCh.jpg?v=1738759187&width=1200", "proteina", "vainilla"),
@@ -42,21 +57,67 @@ INSERT INTO producto(id, nombre, marca, descripcion, precio, stock, imagen, cate
 (NULL, "Keto Pancakes", "Granger", "4g de carbohidratos por porción, siendo muy efectivo en mantener tus objetivos nutricionales. Además son una fuente excelente de proteínas y son aptos para dieta cetogenica.", 12000, 120, "https://acdn-us.mitiendanube.com/stores/001/681/926/products/ketopancakes-vainilla200g-1-6e2bd4f0d7c1adf8d217506903942213-480-0.jpg", "recetas", "vainilla"),
 (NULL, "Cupcakes", "Granger", "15g de proteína por porción con un perfil completo de aminoácidos. Forma practica y rápida de ingerir proteínas, no contiene azucares agregados ni grasas, por lo que no solo te ayuda a crear musculo y a mantenerlo, si no también a quemar grasas gracias a su bajo contenido energético.", 11500, 120, "https://acdn-us.mitiendanube.com/stores/001/681/926/products/cupcakes-31d6919edf69b450f917163829823000-480-0.png", "recetas", "chocolate"),
 (NULL, "Creatina Monohidratada", "Star Nutrition", "La creatina micronizada es la unión de tres aminoácidos que ofrece mayores ventajas que la creatina normal, mejorando su asimilación. La creatina monohidrato es recomendada durante un plan de entrenamiento para aumentar la fuerza, resistencia y recuperación.", 31800, 70, "https://starnutrition.com.ar/cdn/shop/files/CreatineM-300g.png?v=1718218487&width=750", "creatina", "neutro");
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
 
 
---CARGAR PEDIDOS
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+--CARGA DE PEDIDOS
 INSERT INTO pedido(id, id_usuario, fecha_hora, estado, total) VALUES
 (NULL, 1, '2025-01-04 14:30:00', "entregado", 200000),
 (NULL, 2, '2025-06-06 16:30:00', "en_proceso", 56500),
 (NULL, 3, '2025-8-05 22:30:00', "carrito", 13000);
 
+INSERT INTO pedido(id, id_usuario, fecha_hora, estado, total) VALUES
+(4, 4, '2025-01-15 10:23:45', "no_pagado", 84000),
+(5, 5, '2025-02-28 19:05:10', "entregado", 95400),
+(6, 6, '2025-03-12 08:45:00', "carrito", 78000),
+(7, 7, '2025-04-01 14:55:30', "en_proceso", 28500),
+(8, 8, '2025-05-09 22:10:00', "en_proceso", 60200),
+(9, 9, '2025-06-01 16:30:25', "carrito", 137700),
+(10, 10, '2025-06-20 07:50:55', "carrito", 46000);
+
 UPDATE pedido SET total = 183600 WHERE id = 1;
 UPDATE pedido SET total = 76000 WHERE id = 2;
 UPDATE pedido SET total = 194500 WHERE id = 3;
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
 
---CARGAR DETALLES DE PEDIDO
+
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+--CARGA DE DETALLES DE PEDIDO
 INSERT INTO pedido_detalle(id, id_pedido, id_producto, cantidad, precio_uni) VALUES
 (NULL, 1, 5, 4, 45900),
 (NULL, 2, 8, 2, 38000),
 (NULL, 3, 2, 5, 38900);
+
+INSERT INTO pedido_detalle(id, id_pedido, id_producto, cantidad, precio_uni) VALUES
+(4, 4, 17, 7, 12000),
+(5, 5, 20, 3, 31800),
+(6, 6, 11, 6, 13000),
+(7, 7, 12, 1, 28500),
+(8, 8, 14, 2, 30100),
+(9, 9, 5, 3, 45900),
+(10, 10, 19, 4, 11500);
+--------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+
+
+
+--INNER JOIN CON TODAS LAS TABLAS
+SELECT usuario.nombre, 
+usuario.apellido, 
+usuario.email, 
+producto.nombre, 
+producto.sabor, 
+pedido.fecha_hora, 
+pedido_detalle.cantidad, 
+pedido.total
+FROM (((pedido INNER JOIN usuario ON pedido.id_usuario = usuario.id)
+INNER JOIN pedido_detalle ON pedido_detalle.id_pedido = pedido.id)
+INNER JOIN producto ON pedido_detalle.id_producto = producto.id);
+
+
 
